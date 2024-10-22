a45a4a6c1cf09cc1a9c841d399e0b910d90a82a6
Nathan Freire <nathan.freire1403@gmail.com>
2024-10-22 17:26:59 -0300

    calculadora

---
+++ src/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // adicionar um pequeno cabeçalho //
+    printf("---------- Programa mensagem-----------");
+    printf("\nSeja Bem Vindo ao nosso programa\n");
+    printf("---------------- fim ---------------\n");
+
+    return 0;
+}
---
+++ src/hello.c
@@ -0,0 +1,9 @@
+// Importação da biblioteca padrão para o uso de teclado e monitor //
+#include <stdio.h>
+
+int main(){
+    // para exibir uma mensagem na tela iremos usar o comando printf (exibir formatando) //
+    printf("Hello, World!\n");
+    // informar a o compilador que o programa acabou //
+    return 0;
+}
